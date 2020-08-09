bfdec215d378f2fd590a8d50e2139ea38acc46cb
Aleksey Bakin <alexey.bakin@gmail.com>
2020-08-09 16:12:57 +0300

    initial infra commit

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Zounds
+
+![CI](https://github.com/kulti/zounds/workflows/CI/badge.svg)
+[![Coverage](https://coveralls.io/repos/github/kulti/zounds/badge.svg?branch=master)](https://coveralls.io/github/kulti/zounds?branch=master)
+
 Zounds is a game engine experiment for my game with the same name.
 
 ## License
-Zounds is licensed under Apache license version 2.0. See LICENSE file.
 
+Zounds is licensed under Apache license version 2.0. See the LICENSE file.
---
+++ scripts/go_test.sh
@@ -0,0 +1,24 @@
+# /bin/bash
+set -e -o pipefail
+
+PACKAGES=$(go list -f '{{.Name}} {{.Dir}} {{.ImportPath}} {{.TestGoFiles}}{{.XTestGoFiles}}' ./...)
+
+ALL_PACKAGES=""
+IFS_BACKUP=${IFS}
+IFS=$'\n'
+for p in ${PACKAGES}; do
+    name=$(echo $p | cut -f1 -d ' ')
+    dir=$(echo $p | cut -f2 -d ' ')
+    pkg=$(echo $p | cut -f3 -d ' ')
+    tests=$(echo $p| cut -f4 -d ' ')
+
+    ALL_PACKAGES="${ALL_PACKAGES} ${pkg}"
+    if [[ ${tests} == '[][]' ]]; then
+        echo "package ${name}_test" > ${dir}/empty_test.go
+    fi
+done
+
+IFS=${IFS_BACKUP}
+go test -v -mod=vendor -cover -covermode=atomic -coverprofile=coverage.txt ${ALL_PACKAGES} | sed -e '/testing: warning: no tests to run/{N;N;d;}'
+
+echo "${ALL_PACKAGES}"
